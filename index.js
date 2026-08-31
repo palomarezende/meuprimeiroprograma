a4cbb939d5c5785cd3cc229c73bcbf1319c5c203
palomarezende <palomarezendev.aa@gmail.com>
2026-08-31 10:39:21 +0100

    criad o indexe e feito o script

---
+++ index.js
@@ -0,0 +1,24 @@
+let nome = "Mulher maravilha"
+let xp = 2
+let nivel = "";
+
+ if(xp <=1000) {
+    nivel = "Ferro";
+} else if (xp <=2000){
+    nivel = "Bronze";
+} else if (xp <=5000){
+  nivel = "Prata";
+} else if (xp <=7000){
+    nivel = "Ouro";
+} else if (xp <=8000){
+    nivel = "Platina";
+} else if (xp <=9000){
+    nivel = "Acendente";
+} else if (xp <=10000){
+    nivel = imortal
+} else {
+    nivel = "Radiante";
+}
+for (let i = 0; i < 1; i++) {
+    console.log(`O Herói de nome ${nome} está no nível de ${nivel}`);
+}
